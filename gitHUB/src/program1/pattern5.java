639a990ee00c2febc08ddaef73a5baf18632cb87
SaikatPatra <patra.saikat41@gmail.com>
2018-11-07 22:50:54 +0530

    java patten program 1st

---
+++ gitHUB/src/program1/pattern5.java
@@ -0,0 +1,22 @@
+package program1;
+
+public class pattern5 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=1;j<=4;j++)
+			{
+				if(i+j>=5)
+				{
+					System.out.print(i);
+			    }
+				else
+				{
+					System.out.print(" ");
+				}
+	}
+	System.out.println( );		
+}
+}
+}
