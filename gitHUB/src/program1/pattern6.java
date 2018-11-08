44bef83634f70fd7ffd97f5037109414189d649b
SaikatPatra <patra.saikat41@gmail.com>
2018-11-08 12:13:05 +0530

    new program added

---
+++ gitHUB/src/program1/pattern6.java
@@ -0,0 +1,20 @@
+package program1;
+
+public class pattern6 {
+	public static void main(String[] args){
+		for(int i=1;i<=5;i++){
+			for(int j=1;j<=5;j++){
+				if(i+j>=6){
+					System.out.print(j);
+					
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println( );
+		}
+	}
+
+}
